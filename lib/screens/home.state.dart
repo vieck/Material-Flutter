6f5bf07fd6e027303610ca0090337963978925da
vieck <vieck@purdue.edu>
2018-04-19 22:00:59 -0400

    Screen navigation swipe works.

--- lib/screens/home.state.dart
+++ lib/screens/home.state.dart
@@ -3,6 +3,10 @@ import 'package:material_flutter/screens/home.dart';
 
 class HomePageState extends State<HomePage> {
 
+  PageController pageController;
+
+  int currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -10,25 +14,53 @@ class HomePageState extends State<HomePage> {
         children: [
           new Container(color: Colors.red),
           new Container(color: Colors.black),
-          new Container(color: Colors.amber),
         ],
+        onPageChanged: onHomePageChanged,
       ),
-      bottomNavigationBar: getBottomNav(),
+      bottomNavigationBar: new BottomNavigationBar(
+        fixedColor: Colors.red,
+        items: getBottomNavItems(),
+        onTap: onNavigationPress,
+        currentIndex: currentPage,
+      ),
     );
   }
-}
 
-BottomNavigationBar getBottomNav() {
-  return new BottomNavigationBar(
-    fixedColor: Colors.red,
-    items: getBottomNavItems(),
-  );
-}
+  void onHomePageChanged(int currentPage){
+   setState(() {
+      this.currentPage = currentPage;
+   });
+  }
 
-List<BottomNavigationBarItem> getBottomNavItems() {
-  List<BottomNavigationBarItem> bottomNavItems = [
-    new BottomNavigationBarItem(icon: const Icon(Icons.favorite), title: new Text('Favorites')),
-    new BottomNavigationBarItem(icon: const Icon(Icons.history), title: new Text('History')),
-  ];
-  return bottomNavItems;
+  List<BottomNavigationBarItem> getBottomNavItems() {
+    List<BottomNavigationBarItem> bottomNavItems = [
+      new BottomNavigationBarItem(
+          icon: const Icon(Icons.favorite), title: new Text('Favorites')),
+      new BottomNavigationBarItem(
+          icon: const Icon(Icons.history), title: new Text('History')),
+    ];
+    return bottomNavItems;
+  }
+
+  void onNavigationPress(int page){
+    pageController.animateToPage(
+        page,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.ease
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = new PageController();
+  }
+
+
 }
